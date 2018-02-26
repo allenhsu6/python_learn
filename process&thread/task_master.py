dc410e0d78d73b53b324c64c4a9f66551a1dcbf6
allenhsu <34368696+allenhsu6@users.noreply.github.com>
2018-02-26 09:44:35 +0800

    all pythonlearn

---
+++ process&thread/task_master.py
@@ -0,0 +1,36 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import queue,time,random 
+from multiprocessing.managers import BaseManager
+#   启动queue
+task_queue = queue.Queue()
+result_queue = queue.Queue()
+
+
+#   注册网络
+class QueueManager(BaseManager):
+    pass
+
+
+# 把两个queue都注册到网络
+QueueManager.register('get_task_queue', callable(lambda: task_queue))
+QueueManager.register('get_result_queue', callable(lambda: result_queue))
+# 绑定端口，设置验证码
+manager = QueueManager(address=('', 5000), authkey=b'qwe')
+manager.start()
+
+task = manager.get_task_queue()
+result = manager.get_result_queue()
+for i in range(10):
+    n = random.randint(0, 10000)
+    print('task %s is put' % n)
+    task.put(n)
+print('try to get the result...')
+for i in range(10):
+    r = result.get(timeout=10)
+    print('result %s...' % r)
+
+manager.shutdown()
+print('master exit')
